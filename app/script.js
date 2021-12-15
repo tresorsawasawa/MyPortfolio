fcb097f8209a0a6402c3175c30ef775e78abb3c6
zairdon20 <josphat2020@gmail.com>
2021-12-15 13:59:46 +0200

    Update script.js >> add array of popup window container

--- app/script.js
+++ app/script.js
@@ -1,6 +1,34 @@
 const menu = document.querySelector('.header');
 const humburger = document.querySelector('#humburger');
 const closeMemu = document.querySelector('#close-menu');
+const main = document.querySelector('main');
+const html = document.querySelector('html');
+const CardContainer = document.querySelector('.card-container');
+const mobileData = [
+  // Array of data implenented in the popup window
+  {
+    id: 'project-1',
+    title: "Tresor's Portofolio",
+    images: './images/app_screenshot.png',
+  },
+  {
+    techno: ['JavaScript', 'Html', 'Css', 'Github', 'Boostrap', 'Terminal'],
+    heading: "Tresor's Portofolio",
+    list: [
+      {
+        icon: './images/icons/IconExport',
+        text: 'See live',
+        live: 'https://tresorsawasawa.github.io/MyPortfolio',
+      },
+      {
+        icon: './images/icons/IconGithub',
+        text: 'See Source',
+        live: 'https://tresorsawasawa.github.io/MyPortfolio',
+      },
+    ],
+    paragraph: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Praesentium exercitationem aliquam tenetur incidunt temporibus!',
+  },
+];
 
 // Add the Change-menu class to open the and close the mobile menu
 
@@ -17,3 +45,4 @@ window.addEventListener('mouseup', (e) => {
     menu.classList.remove('change-menu');
   }
 });
+
