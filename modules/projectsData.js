873c0dd1c27268f57494c3dc3ad2b58fe008f675
tresorsawasawa <tresorsawasawa@gmail.com>
2022-04-09 13:55:29 +0200

    Add a module file for projects data

---
+++ modules/projectsData.js
@@ -0,0 +1,149 @@
+const projectsData = [
+  // Array of data implenented in the popup window
+  {
+    id: 'project1',
+    secondTitle: 'Leaderboard',
+    images: {
+      img: './images/leaderboard.png',
+      closePop: './images/icons/Close_icon_dark.png',
+    },
+    techno: ['JavaScript', 'HTML', 'SCSS', 'Webpack'],
+    heading: 'Leaderboard',
+    list: [
+      {
+        icon: './images/icons/IconExport.svg',
+        text: 'See Live',
+        live: 'https://tresorsawasawa.github.io/Leaderboard_App/dist/',
+      },
+      {
+        text: 'See Source',
+        icon: './images/icons/IconGitHub.svg',
+        live: 'https://github.com/tresorsawasawa/Leaderboard_App',
+      },
+    ],
+    paragraph:
+      '"Leaderboard" is a website displays scores submitted by different players. It also allows you to submit your score. All data is preserved thanks to the external Leaderboard API service.',
+  },
+  {
+    id: 'project2',
+    secondTitle: 'Awesome Books',
+    images: {
+      img: './images/Desktop-screenshot.png',
+      closePop: './images/icons/Close_icon_dark.png',
+    },
+    techno: ['JavaScript', 'HTML', 'CSS', 'Boostrap'],
+    heading: 'Awesome Books',
+    list: [
+      {
+        icon: './images/icons/IconExport.svg',
+        text: 'See Live',
+        live: 'https://tresorsawasawa.github.io/Awesome_books/',
+      },
+      {
+        text: 'See Source',
+        icon: './images/icons/IconGitHub.svg',
+        live: 'https://github.com/tresorsawasawa/Awesome_books',
+      },
+    ],
+    paragraph:
+    '"Awesome books" is a simple website that displays a list of books and allows you to add, remove books from that list and store your books list in your browser local storage.',
+  },
+  {
+    id: 'project3',
+    secondTitle: 'Movie Infos App',
+    images: {
+      img: './images/movie-app.png',
+      closePop: './images/icons/Close_icon_dark.png',
+    },
+    techno: ['JavaScript', 'HTML', 'CSS', 'Boostrap'],
+    heading: 'Movie Infos App',
+    list: [
+      {
+        icon: './images/icons/IconExport.svg',
+        text: 'See Live',
+        live: 'https://tresorsawasawa.github.io/Movie-App/',
+      },
+      {
+        text: 'See Source',
+        icon: './images/icons/IconGitHub.svg',
+        live: 'https://github.com/tresorsawasawa/Movie-App',
+      },
+    ],
+    paragraph:
+      'Movie Infos App is a web page that, not only, can help you to track every movie and informations about the movie, but also it can help you be updated about popular and new movies',
+  },
+  {
+    id: 'project4',
+    secondTitle: 'Solidarty Up!',
+    images: {
+      img: './images/solidarity_up.png',
+      closePop: './images/icons/Close_icon_dark.png',
+    },
+    techno: ['JavaScript', 'HTML', 'CSS', 'Boostrap'],
+    heading: 'Solidarty Up!',
+    list: [
+      {
+        icon: './images/icons/IconExport.svg',
+        text: 'See Live',
+        live: 'https://tresorsawasawa.github.io/Solidarity-Capstone-project/',
+      },
+      {
+        text: 'See Source',
+        icon: './images/icons/IconGitHub.svg',
+        live: 'https://github.com/tresorsawasawa/Solidarity-Capstone-project',
+      },
+    ],
+    paragraph:
+      'Solidarity Up is a web page of simple facts that would be better to be practiced in our everyday life in the aim of making people aware of helping others, Reminding everyone to understand their value in society, Understand that you can help others regardless of the field and these will help to make world better.',
+  },
+  {
+    id: 'project5',
+    secondTitle: 'MyPortfolio',
+    images: {
+      img: './images/Portfoli1_Screenshot.png',
+      closePop: './images/icons/Close_icon_dark.png',
+    },
+    techno: ['Javascript', 'Html', 'Css'],
+    heading: "Tresor's Portfolio",
+    list: [
+      {
+        icon: './images/icons/IconExport.svg',
+        text: 'See Live',
+        live: 'https://tresorsawasawa.github.io/My_Portfolio/',
+      },
+      {
+        text: 'See Source',
+        icon: './images/icons/IconGitHub.svg',
+        live: 'https://github.com/tresorsawasawa/My_Portfolio',
+      },
+    ],
+    paragraph:
+    'This is my personal portfolio that describes myself and what I can do; like  build products for the WEB, feature or website Look through some of my work and experience! If you like what you see and have a project you need coded, don’t hestiate to contact me.',
+  },
+  {
+    id: 'project6',
+    secondTitle: 'Todo List App',
+    images: {
+      img: './images/Todo_Screenshot.png',
+      closePop: './images/icons/Close_icon_dark.png',
+    },
+    techno: ['Javascript', 'Html', 'Css', 'Webpack'],
+    heading: 'Todo List App',
+    list: [
+      {
+        icon: './images/icons/IconExport.svg',
+        text: 'See Live',
+        live: 'https://tresorsawasawa.github.io/Todo_List_App/',
+      },
+      {
+        text: 'See Source',
+        icon: './images/icons/IconGitHub.svg',
+        live: 'https://github.com/tresorsawasawa/Todo_List_App',
+      },
+    ],
+    paragraph:
+    "A To-do list is a tool that helps to organize your day. It simply lists the things that you need to do and allows you to mark them as complete. And by prioritizing tasks, you plan the order in which you'll do them so that you can tell what needs your immediate attention, and what you can leave until later.",
+  },
+];
+
+export default projectsData;
